7517dc57e622bc1bdb4c856cffd9522b266d82ab
Senguana Wisuma Emilio Yankuam <Emilio2019s>
2019-11-26 09:34:06 -0500

    MODIFICADO CARPETA CERTIFICADO

---
+++ vistas/certificado/certificado_plantilla.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Certificado</title>
+    <!-- <link rel="stylesheet" href="style.css"> -->
+    <link rel="stylesheet" type="text/css" href="../../assets/css/azzara.css">
+</head>
+<body>
+	<div class="col-md-6 ml-auto mr-auto">
+		<table>
+			<tr>
+				<div class="user-profile text-center">
+					<div class="name">Hizrian, 19</div>
+					<p class="dni"><b><h1>OTORGA EL PRESENTE CERTIFICADO A:</h1></b></p>
+					<span class="h2">SENGUANA WISUMA EMILIO YANKUAM</span>
+					<p><b><h1>C.I.1450152325</h1></b></p>
+					
+				</div>
+			</tr>
+			<tbody>
+				<tr>
+					<td>data</td>
+				</tr>
+			</tbody>
+		</table>
+		
+	</div>
+<div id="page_pdf">
+	
+	<table id="factura_cliente">
+		<tr>
+			<td class="info_cliente">
+				<div class="round">
+					<span class="h3">Por haber aprobado el curso:</span>
+					<table class="datos_cliente">
+						<tr>
+							<td>PROGRAMACIÓN PHP</td>
+						</tr>
+					</table>
+				</div>
+			</td>
+
+		</tr>
+	</table>
+	<div>
+		<p class="nota">Si usted tiene preguntas sobre esta factura, <br>pongase en contacto con nombre, teléfono y Email</p>
+		<h4 class="label_gracias">¡Gracias por su compra!</h4>
+	</div>
+
+</div>
+
+</body>
+</html>
